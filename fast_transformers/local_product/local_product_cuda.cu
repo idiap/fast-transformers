d4f2da49374b12ce70d7dc8b071202921aa868cf
Angelos Katharopoulos <katharas@gmail.com>
2020-09-28 18:58:03 +0200

    Refactor the sliding dot product

--- fast_transformers/local_product/local_product_cuda.cu
+++ fast_transformers/local_product/local_product_cuda.cu
@@ -4,11 +4,13 @@
 //
 
 #include <limits>
+#include <functional>
 
 #include <torch/extension.h>
 
 
 typedef torch::PackedTensorAccessor32<float, 4, torch::RestrictPtrTraits> float4_accessor;
+typedef torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> float3_accessor;
 typedef torch::PackedTensorAccessor32<float, 2, torch::RestrictPtrTraits> float2_accessor;
 typedef torch::PackedTensorAccessor32<long, 1, torch::RestrictPtrTraits> long_accessor;
 
@@ -17,117 +19,183 @@ inline int ceildiv(int a, int b) {
     return (a + b - 1)/b;
 }
 
-
-template <int LB=32, int KB=32, int EB=32>
-__global__ void sliding_dot(
-    float4_accessor queries,
-    float4_accessor keys,
-    float4_accessor output,
-    dim3 strides
-) {
-    int idx = blockIdx.x;
-    int n = idx / strides.x;
-    idx -= n*strides.x;
-    int h = idx / strides.y;
-    idx -= h*strides.y;
-    int lblock = idx / strides.z;
-    idx -= lblock*strides.z;
-    int kblock = idx;
-
-    int E = keys.size(3);
-    int local_context = output.size(3);
-
-    int l_local = threadIdx.x / EB;
-    int k_local = threadIdx.x - l_local*EB;
-    int l = lblock * LB + l_local;
-    int k = kblock * KB + k_local;
-    int s = l - local_context/2 + k;
-    int s2 = s + LB;
-
-    extern __shared__ float shared_mem[];
-    float * s_queries = shared_mem;
-    float * s_keys = s_queries + LB*EB;
-
-    if (n > queries.size(0)) {
-        return;
-    }
-
-    for (int e=0; e<E; e+=EB) {
-        // Load the queries and keys
-        if (e + k_local < E) {
-            if (l < queries.size(2)) {
-                s_queries[l_local*EB + k_local] = queries[n][h][l][e+k_local];
-            } else {
-                s_queries[l_local*EB + k_local] = 0;
-            }
-            {
-                int s = lblock * LB + l_local - local_context/2 + kblock * KB;
-                if (s >= 0 && s < keys.size(2)) {
-                    s_keys[l_local*EB + k_local] = keys[n][h][s][e+k_local];
-                } else {
-                    s_keys[l_local*EB + k_local] = 0;
-                }
-                int s2 = s + LB;
-                if (s2 >= 0 && s2 < keys.size(2)) {
-                    s_keys[(l_local + LB)*EB + k_local] = keys[n][h][s2][e+k_local];
-                } else {
-                    s_keys[(l_local + LB)*EB + k_local] = 0;
-                }
-            }
-        } else {
-            s_queries[l_local*EB + k_local] = 0;
-            s_keys[l_local*EB + k_local] = 0;
-            s_keys[(l_local + LB)*EB + k_local] = 0;
-        }
-        __syncthreads();
-        
-        // Produce the nhlk output
-        float result = 0;
-        #pragma unroll
-        for (int e_local=0; e_local<EB; e_local++) {
-            result += s_queries[l_local*EB + e_local] * s_keys[(l_local+k_local)*EB + e_local];
-        }
-        __syncthreads();
-
-        if (l < queries.size(2) && k < local_context) {
-            output[n][h][l][k] += result;
-        }
-    }
-}
-
-
-__global__ void copy_masked(
-    float4_accessor buffer,
-    float2_accessor attn_mask,
-    long_accessor key_lengths,
-    float4_accessor output,
+template <typename copy_implementation>
+__global__ void sliding_dot_copy_kernel(
+    copy_implementation copy,
+    float3_accessor buffer,
+    float3_accessor output,
     int local_context,
     int l_start,
-    int s_start
+    int s_start,
+    int buffer_dim12,
+    int buffer_dim2
 ) {
-    int idx = blockIdx.x*blockDim.x + threadIdx.x;
-    int n = idx / buffer.stride(0);
-    idx = idx - n*buffer.stride(0);
-    int h = idx / buffer.stride(1);
-    idx = idx - h*buffer.stride(1);
-    int l_offset = idx / buffer.stride(2);
-    idx = idx - l_offset*buffer.stride(2);
-    int s_offset = idx;
-
-    if (n >= buffer.size(0)) {
-        return;
-    }
-
-    int l = l_start + l_offset;
-    int s = s_start + s_offset;
-    int k = s - l + local_context/2;
-
-    if (k < 0 || k >= local_context) {
-        return;
-    }
-
-    output[n][h][l][k] = buffer[n][h][l_offset][s_offset] + attn_mask[l][s];
+    copy(
+        buffer,
+        output,
+        local_context,
+        l_start,
+        s_start,
+        buffer_dim12,
+        buffer_dim2
+    );
 }
+
+/**
+ * Multiply every A_i with every B_j iff |i-j| < local_context/2.
+ *
+ * The strategy is to compute the local products in blocks and keep the GPU
+ * busy and then select the valid results from all the intermediate ones.
+ *
+ * The naming means that both arguments span the full sequences, namely both A
+ * and B are the global matrices.
+ *
+ * Arguments
+ * ---------
+ *     A: (N, L, E)
+ *     B: (N, L, E)
+ *     out: (N, L, local_context)
+ */
+template <int a_blocks=64, typename CopyImplementation>
+void sliding_dot(
+    CopyImplementation copy_implementation,
+    torch::Tensor A,
+    torch::Tensor B,
+    torch::Tensor out,
+    int local_context
+) {
+    int N = A.size(0);
+    int L = A.size(1);
+
+    // Save the intermediate results in here
+    auto buffer = A.new_zeros({N, a_blocks, a_blocks+local_context});
+
+    for (int l=0; l<L; l+=a_blocks) {
+        // Compute the sizes of the sub problems to be computed in this
+        // block iteration
+        int s_start = std::max(0, l-local_context/2);
+        int s_end = std::min(L, l-local_context/2+local_context+a_blocks);
+        int n_b = s_end-s_start;
+        int n_a = std::min(L-l, a_blocks);
+
+        // Compute the dot products
+        auto buff = buffer.narrow(1, 0, n_a).narrow(2, 0, n_b);
+        at::matmul_out(
+            buff,
+            A.narrow(1, l, n_a),
+            B.narrow(1, s_start, n_b).transpose(1, 2)
+        );
+
+        // Select the correct results from the buffer
+        const int threads = 1024;
+        int blocks = ceildiv(buff.numel(), threads);
+        sliding_dot_copy_kernel<<<blocks, threads>>>(
+            copy_implementation,
+            buff.packed_accessor32<float, 3, torch::RestrictPtrTraits>(),
+            out.packed_accessor32<float, 3, torch::RestrictPtrTraits>(),
+            local_context,
+            l,
+            s_start,
+            buff.size(1)*buff.size(2),
+            buff.size(2)
+        );
+    }
+}
+
+
+/**
+ * This copy implementation simply copies the appropriate values from the
+ * buffer and adds the corresponding value from the attention mask.
+ */
+struct masked_lp_copy
+{
+    float2_accessor attn_mask;
+    long_accessor key_lengths;
+
+    masked_lp_copy(float2_accessor _attn_mask, long_accessor _key_lengths) :
+        attn_mask(_attn_mask), key_lengths(_key_lengths)
+        {}
+
+    __device__ void operator()(
+        float3_accessor buffer,
+        float3_accessor output,
+        int local_context,
+        int l_start,
+        int s_start,
+        int buffer_dim12,
+        int buffer_dim2
+    ) {
+        int idx = blockIdx.x*blockDim.x + threadIdx.x;
+        int n = idx / buffer_dim12;
+        idx = idx - n*buffer_dim12;
+        int l_offset = idx / buffer_dim2;
+        idx = idx - l_offset*buffer_dim2;
+        int s_offset = idx;
+
+        if (n >= buffer.size(0)) {
+            return;
+        }
+
+        int l = l_start + l_offset;
+        int s = s_start + s_offset;
+        int k = s - l + local_context/2;
+
+        if (k < 0 || k >= local_context) {
+            return;
+        }
+
+        output[n][l][k] = buffer[n][l_offset][s_offset] + attn_mask[l][s];
+    }
+
+    static masked_lp_copy factory(
+        torch::Tensor attn_mask,
+        torch::Tensor key_lengths
+    ) {
+        return masked_lp_copy(
+            attn_mask.packed_accessor32<float, 2, torch::RestrictPtrTraits>(),
+            key_lengths.packed_accessor32<long, 1, torch::RestrictPtrTraits>()
+        );
+    }
+};
+
+
+/**
+ * The simplest copy implementation just copies the values if they are within
+ * bounds.
+ */
+struct lp_copy
+{
+    __device__ void operator()(
+        float3_accessor buffer,
+        float3_accessor output,
+        int local_context,
+        int l_start,
+        int s_start,
+        int buffer_dim12,
+        int buffer_dim2
+    ) {
+        int idx = blockIdx.x*blockDim.x + threadIdx.x;
+        int n = idx / buffer_dim12;
+        idx = idx - n*buffer_dim12;
+        int l_offset = idx / buffer_dim2;
+        idx = idx - l_offset*buffer_dim2;
+        int s_offset = idx;
+
+        if (n >= buffer.size(0)) {
+            return;
+        }
+
+        int l = l_start + l_offset;
+        int s = s_start + s_offset;
+        int k = s - l + local_context/2;
+
+        if (k < 0 || k >= local_context) {
+            return;
+        }
+
+        output[n][l][k] = buffer[n][l_offset][s_offset];
+    }
+};
 
 
 template <int LB=32, int KB=32, int EB=32>
@@ -306,33 +374,14 @@ torch::Tensor local_dot_product(
         {N, H, L, local_context},
         -std::numeric_limits<float>::infinity()
     );
-    const int blocks = 64;
-    auto buffer = queries.new_zeros({N, H, blocks, blocks+local_context});
 
-    const int threads = 1024;
-    int cuda_blocks = ceildiv(buffer.numel(), threads);
-
-    for (int l=0; l<L; l+=blocks) {
-        int s_start = std::max(0, l-local_context/2);
-        int s_end = std::min(S, l-local_context/2+local_context+blocks);
-        int n_keys = s_end-s_start;
-        int n_queries = std::min(L-l, blocks);
-        auto buff = buffer.narrow(3, 0, n_keys).narrow(2, 0, n_queries);
-        at::matmul_out(
-            buff,
-            queries.narrow(2, l, n_queries),
-            keys.narrow(2, s_start, n_keys).transpose(2, 3)
-        );
-        copy_masked<<<cuda_blocks, threads>>>(
-            buff.packed_accessor32<float, 4, torch::RestrictPtrTraits>(),
-            attn_mask.packed_accessor32<float, 2, torch::RestrictPtrTraits>(),
-            key_lengths.packed_accessor32<long, 1, torch::RestrictPtrTraits>(),
-            output.packed_accessor32<float, 4, torch::RestrictPtrTraits>(),
-            local_context,
-            l,
-            s_start
-        );
-    }
+    sliding_dot(
+        masked_lp_copy::factory(attn_mask, key_lengths),
+        queries.view({N*H, L, E}),
+        keys.view({N*H, L, E}),
+        output.view({N*H, L, local_context}),
+        local_context
+    );
 
     return output;
 }
@@ -461,25 +510,13 @@ std::tuple<torch::Tensor, torch::Tensor> local_weighted_average_backward(
     }
 
     // Compute the gradient wrt to the attention
-    {
-        const int threads = 32*32;
-        int lblocks = ceildiv(L, 32);
-        int kblocks = ceildiv(local_context, 32);
-        int blocks = N * H * lblocks * kblocks;
-        int shared_mem = 32*32 * 3 * sizeof(float);
-        dim3 strides(
-            H*lblocks*kblocks,
-            lblocks*kblocks,
-            kblocks
-        );
-
-        sliding_dot<<<blocks, threads, shared_mem>>>(
-            grad.packed_accessor32<float, 4, torch::RestrictPtrTraits>(),
-            values.packed_accessor32<float, 4, torch::RestrictPtrTraits>(),
-            grad_attention.packed_accessor32<float, 4, torch::RestrictPtrTraits>(),
-            strides
-        );
-    }
+    sliding_dot(
+        lp_copy(),
+        grad.view({N*H, L, E}),
+        values.view({N*H, L, E}),
+        grad_attention.view({N*H, L, local_context}),
+        local_context
+    );
 
     return std::make_tuple(grad_attention, grad_values);
 }
